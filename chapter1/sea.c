7c5fcba1866eeaacc70d3e681632cc36d0103a70
junsuPark <krpjs0508@gmail.com>
2025-10-21 02:41:38 +0900

    c

--- chapter1/sea.c
+++ chapter1/sea.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+// int main()
+// {
+//     printf("from sea to shining C\n");
+//     return 0;
+// }
+
 int main()
 {
-    printf("from sea to shining C\n");
+    printf("from sea to ");
+    printf("shining C");
+    printf("\n");
     return 0;
 }
